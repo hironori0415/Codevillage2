aa6a94d6e5ec62604259bace08f690cfed77e151
hironori0415 <hironori.oohara@gmail.com>
2020-12-04 05:14:44 +0900

    修正点 ・1024pxよりも大きいサイズだと右側に余白を修正 ・スマホサイズの時のfirstviewの文字をもう少し上修正 ・menuの下の白の余白をもう少しだけ広く修正
    ・650px時にfirstviewが少し崩れてしまっているので、修正修正
    ・700px時のaccessの地図の比率を修正

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(function () {
+  //ハンバーガーメニュー
+  $(".hamburger-btn").click(function () {
+    $(this).toggleClass("active");
+
+    if ($(this).hasClass("active")) {
+      $(".hamburger").addClass("active");
+    } else {
+      $(".hamburger").removeClass("active");
+    }
+  });
+  $(".hamburger__list").click(function () {
+    $(".hamburger,.hamburger-btn").removeClass("active");
+  });
+}
